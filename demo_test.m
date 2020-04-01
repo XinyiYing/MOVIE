facf1e312e4c3bcf0845594f7f83008ed7e84fab
Xinyi Ying <yingxinyi18@nudt.edu.cn>
2020-04-01 17:51:22 +0800

    Add files via upload

---
+++ demo_test.m
@@ -0,0 +1,22 @@
+%% demo_test
+% run_movie_image('test_data\im\', [])
+% Parameters:
+% test images path . if empty, automatically set 'test_data\im\'
+% required heigth and weigth of images.if empty, automatically set the width and height of images.
+% -------------------note-------------------------
+% 1. The size of reference images and distorted images path should be the same.
+% 2. Reference images should be put in 'test_data\im\calendar\hr'. 'calendar' is the scene name.
+% 3. Distorted images should be put in 'test_data\im\calendar\sr'.
+% 4. You can check the generated *.yuv in '\output_data\video\calendar\' to ensure *.yuv is accurate (not consist of noise).
+% 5. To open *.yuv, you can use the yuvplayer.exe in '\tool'. Before play *.yuv, you should set weigth and height of video in yuvplayer.exe
+% 6. The size of output *,yuv is smaller than the input images, we crop the 10*10 boundary of the input images.
+run_movie_video('test_data\video\', [])
+% Parameters: 
+% test videos path. if empty, automatically set 'test_data\video\'
+% required heigth and weigth of videos. if empty, automatically set the width and height of videos.
+% -------------------note-------------------------
+% 1. The size of reference videos and distorted videos path should be the same.
+% 2. Movies should be put in 'test_data\video\city'. 'city' is the scene name.
+% 3. 'hr.avi' and 'sr.avi' is the reference video and the distorted video. Code support other video format, we recommand *.avi.
+% 4. You can check the generated *.yuv in '\output_data\video\city\' to ensure *.yuv is accurate (not consist of noise).
+% 5. To open *.yuv, you can use the yuvplayer.exe in '\tool'. Before play *.yuv, you should set weigth and height of video in yuvplayer.exe
---
+++ im2yuv.m
@@ -0,0 +1,81 @@
+function [H,W]=im2yuv(img_dir, img_size)
+% H, W is the width and height of test images (or required szie of test_image cropped from the orignal data).
+% hr_dir is the path of images folder. 
+%% im2avi
+output_dir = 'output_data'; % path to save output data (*.avi, *.yuv)
+mkdir(output_dir);
+mkdir(output_dir,'\video');
+temp0 = strsplit(img_dir, '\');
+scene = temp0{3};
+name = temp0{4};
+mkdir([output_dir,'\video\', scene]);
+aviobj  = VideoWriter([output_dir,'\video\',scene,'\',name,'.avi']); % path to save *.avi
+% standard metric for video
+aviobj.Quality = 100;
+aviobj.FrameRate = 30;
+open(aviobj)
+
+img_list = dir(img_dir);
+temp0 = img_list(length(img_list)-1).name;
+frame0 = imread([img_dir, '\', temp0]);
+temp0 = strsplit(temp0, '.');
+png = temp0{2};
+if img_size
+    W = img_size(1);
+    H = img_size(2);
+else
+    [H,W,C] = size(frame0);
+end
+img_list =  dir([img_dir, '\*.', png]);
+for i=1:length(img_list)
+    fname = [img_dir, '\', img_list(i).name];
+    frame = imread(fname);
+    frame_ycbcr = rgb2ycbcr(frame);
+    frame_ycbcr(:,:,2) = frame_ycbcr(:,:,1);
+    frame_ycbcr(:,:,3) = frame_ycbcr(:,:,1);
+            
+    frame = frame_ycbcr(1:H, 1:W, :); % Crop the input images according to H, W
+    frame = frame(11:end-10, 11:end-10, :); % Crop the 10*10 boundary
+    % MOVIE metric require the length of frame over 33
+    if length(img_list) < 33
+        temp = 33 - length(img_list);
+        if mod(temp, 2) == 0
+            if i == 1
+                for j = 1:temp/2
+                    writeVideo(aviobj,frame);
+                end
+                writeVideo(aviobj,frame);
+            elseif i == 31
+                for j = 1:temp/2
+                    writeVideo(aviobj,frame);
+                end
+                writeVideo(aviobj,frame);
+            else
+                writeVideo(aviobj,frame);
+            end
+        else
+            if i == 1
+                for j = 1:int32(temp/2)
+                    writeVideo(aviobj,frame);
+                end
+                writeVideo(aviobj,frame);
+            elseif i == 31
+                for j = 1:int32(temp/2)-1
+                    writeVideo(aviobj,frame);
+                end
+                writeVideo(aviobj,frame);
+            else
+                writeVideo(aviobj,frame);
+            end
+        end
+    else
+        writeVideo(aviobj,frame);
+    end
+end
+close(aviobj); 
+
+%% avi2yuv
+avi_dir = [output_dir,'\video\',scene,'\',name,'.avi'];
+yuv_dir = [output_dir,'\video\',scene,'\',name,'.yuv'];
+cmd = ['tool\ffmpeg.exe',' -i ',avi_dir,' -pix_fmt yuv420p ',yuv_dir];
+system(cmd)
